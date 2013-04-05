84a3705903c5641a7865d1d5637e0146eeff63dd
John Lawrence Aspden <github@aspden.com>
2013-04-05 19:00:28 +0100

    lein skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simple-plotter
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/simple_plotter/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-plotter.core-test
+  (:use clojure.test
+        simple-plotter.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
