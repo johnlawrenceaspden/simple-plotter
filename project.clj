6c84506bbb45b7b87657abd143b3b7b0e3763b0d
John Lawrence Aspden <github@aspden.com>
2013-04-14 19:23:49 +0100

    looks stable. ship it.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-plotter "0.1.1-SNAPSHOT"
+(defproject simple-plotter "0.1.1"
   :description "Graphics like it's 1979"
   :url "http://github.com/johnlawrenceaspden/simple-plotter"
   :license {:name "Eclipse Public License"
