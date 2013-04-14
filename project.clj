f5e4eedc13cde02bc27e67d09019950bfb2bd4b1
John Lawrence Aspden <github@aspden.com>
2013-04-14 21:46:45 +0100

    instructions for pushing to clojars (using ssh key only)

--- project.clj
+++ project.clj
@@ -4,3 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
+
+
+;; lein clean
+;; lein pom
+;; lein jar
+;; scp pom.xml target/simple-plotter-0.1.1.jar clojars@clojars.org:
